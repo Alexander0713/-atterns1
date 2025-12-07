08a1d3c7f545bbad85c9d98b6a4073a504c9ea24
Alexandr <0231050@mail.ru>
2025-12-07 19:15:06 +0300

    InitialCommit

---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,76 @@
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private static final String[] CITIES = {
+            "Москва", "Санкт-Петербург", "Казань", "Новосибирск", "Екатеринбург",
+            "Нижний Новгород", "Челябинск", "Самара", "Омск", "Ростов-на-Дону",
+            "Уфа", "Красноярск", "Воронеж", "Пермь", "Волгоград"
+    };
+
+    private static final Random random = new Random();
+
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        LocalDate date = LocalDate.now().plusDays(shift);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return date.format(formatter);
+    }
+
+    public static String generateCity() {
+        return CITIES[random.nextInt(CITIES.length)];
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.numerify("+79#########");
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(
+                    generateCity(),
+                    generateName(locale),
+                    generatePhone(locale)
+            );
+        }
+    }
+
+    public static class UserInfo {
+        private final String city;
+        private final String name;
+        private final String phone;
+
+        public UserInfo(String city, String name, String phone) {
+            this.city = city;
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+    }
+}
---
+++ src/test/java/DeliveryTest.java
@@ -0,0 +1,50 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static org.openqa.selenium.Keys.BACK_SPACE;
+
+public class DeliveryTest {
+    private DataGenerator.UserInfo user;
+
+    @BeforeEach
+    void setUp() {
+        Configuration.headless = false; // для локальной отладки
+        Configuration.timeout = 15000;
+        open("http://localhost:9999");
+        user = DataGenerator.Registration.generateUser("ru");
+    }
+
+    @Test
+    void shouldTestDeliveryCard() {
+        // 1. Первая запись
+        $("[data-test-id=city] input").setValue(user.getCity());
+        $("[data-test-id=date] input").doubleClick().press(BACK_SPACE);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(4));
+        $("[data-test-id=name] input").setValue(user.getName());
+        $("[data-test-id=phone] input").setValue(user.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+
+        $("[data-test-id=success-notification]")
+                .shouldBe(visible, Duration.ofSeconds(15));
+
+        // 2. Изменяем дату
+        $("[data-test-id=date] input").doubleClick().press(BACK_SPACE);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(7));
+        $(byText("Запланировать")).click();
+
+        $("[data-test-id=replan-notification]")
+                .shouldBe(visible, Duration.ofSeconds(15));
+
+        $("[data-test-id=replan-notification] button").click();
+
+        $("[data-test-id=success-notification]")
+                .shouldBe(visible, Duration.ofSeconds(15));
+    }
+}
